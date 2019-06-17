6ea37703d894238c36605e02502dca104f11fabd
Josaphat Valdivia <jos@josaphat.co>
2019-06-16 20:22:47 -0400

    Add a super simple `yes` utility
    The uutils implementation does a bunch of stuff with Cow and and
    buffering and... well, it all just serves to complicate how simple
    this program actually is.

---
+++ src/bin/yes.rs
@@ -0,0 +1,37 @@
+//! yes - output a string repeatedly until killed
+
+use clap::{App, Arg};
+use std::io;
+use std::io::prelude::*;
+use std::process;
+
+fn main() {
+    let matches = App::new("rust-yes")
+        .version("0.1.0")
+        .author("Jos V. <jos@josaphat.co>")
+        .about("Prints the given arguments forever until killed.")
+        .arg(
+            Arg::with_name("string")
+                .value_name("STRING")
+                .default_value("y")
+                .multiple(true),
+        )
+        .get_matches();
+    let string = matches
+        .values_of("string")
+        .unwrap()
+        .collect::<Vec<&str>>()
+        .join(" ");
+
+    // We'd like to just use the println! macro, but this doesn't
+    // exhibit graceful behavior when part of a pipeline that gets
+    // closed.
+    // We'll use the write! family of macros instead, quietly exiting
+    // the program in case of errors.
+    let mut out = io::stdout();
+    loop {
+        writeln!(out, "{}", string).unwrap_or_else(|_| {
+            process::exit(0);
+        });
+    }
+}
